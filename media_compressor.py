ad42097d7058b916962b8c4a643ea99a82109d18
TimurMeshchenko <matsuokisho@gmail.com>
2024-04-13 20:13:07 +0300

    release

--- media_compressor.py
+++ media_compressor.py
@@ -1,5 +1,6 @@
 from moviepy.editor import VideoFileClip
 from PIL import Image
+import os
 
 def compress_video(input_file, output_file):
     video_clip = VideoFileClip(input_file)
@@ -10,10 +11,24 @@ def compress_image(input_image, output_image, quality=25):
     with Image.open(input_image) as img:
         img.save(output_image, quality=quality, optimize=True)
 
-input_file = 'minecraft.mp4'
-output_file = 'compressed_video.mp4'
-compress_video(input_file, output_file)
+def get_files_in_dir(base_directory, take_from_directory_name):
+    directory = os.path.join(base_directory, take_from_directory_name)
+    files = os.listdir(directory)
+    for file_name in files:
+        file_path = os.path.join(directory, file_name)
+        output_file = file_name.replace("SaveTube.io-", "").replace("_", "").replace("  ", " ")
+        compressed_directory = os.path.join(base_directory, get_filename_without_extension(output_file))
+        file_compressed_path = os.path.join(compressed_directory, output_file)
+        os.makedirs(compressed_directory, exist_ok=True)
+        if file_name.endswith('mp4'):
+            compress_video(file_path, file_compressed_path)
+        elif file_name.endswith('jpg'):
+            compress_image(file_path, file_compressed_path)
 
-input_image = 'minecraft.jpg'
-output_image = 'compressed_image.jpg'
-compress_image(input_image, output_image)
+def get_filename_without_extension(filename: str):
+    filename_with_extension = filename.split('/')[-1]
+    filename_parts = filename_with_extension.split('.')
+    filename_without_extension = '.'.join(filename_parts[:-1])
+    return filename_without_extension
+
+get_files_in_dir("src/media/videos/", "Folder2")
